5b6120a61866cd56f25157e91a64bc1a48b38361
2935601977 <114342651+2935601977@users.noreply.github.com>
2024-10-16 12:37:28 +0800

    Add files via upload

---
+++ IM即时通信软件/src/client/userinfo.cpp
@@ -0,0 +1,11 @@
+﻿#include "userinfo.h"
+
+userinfo::userinfo()
+{
+    this->islogin = false;
+//    this->ip = "10.171.213.244";
+    this->ip = "192.168.31.228";
+    this->port = 8888;
+}
+
+userinfo user;
